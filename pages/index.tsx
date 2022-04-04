c0127ff2966c5947aeac51a7f81c15d20fecf428
Sanjib Kumar Sen <sksenonline@gmail.com>
2022-04-04 06:18:52 +0600

    formatting, Link optimized

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,48 +3,51 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import get_stock from "../scripts/get-stock-price";
 import get_env_data from "../scripts/get-env-data";
+import Link from "next/link";
 
 type Props = {
   data: {
-    price: number,
-    stock: number,
-    vat: number,
-    sell: string,
-    profit: string,
-    profit_money: number,
-    buy_per_share: number,
-    target: number,
-    company: string
+    price: number;
+    stock: number;
+    vat: number;
+    sell: string;
+    profit: string;
+    profit_money: number;
+    buy_per_share: number;
+    target: number;
+    company: string;
   };
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const price = await get_stock();
   const env_data = get_env_data();
-  const buy = env_data.buy
-  let profit = "লাভ"
-  const sell = Math.floor((Number(env_data.stock) *Number(price)) - Number(env_data.vat))
-  if (Number(buy)>Number(sell)){
-    profit= "লস"
+  const buy = env_data.buy;
+  let profit = "লাভ";
+  const sell = Math.floor(
+    Number(env_data.stock) * Number(price) - Number(env_data.vat)
+  );
+  if (Number(buy) > Number(sell)) {
+    profit = "লস";
   }
   const data = {
     price: price,
     stock: env_data.stock,
     vat: env_data.vat,
     sell: sell.toLocaleString(),
-    profit:profit,
-    profit_money: Math.abs(sell-Number(buy)),
+    profit: profit,
+    profit_money: Math.abs(sell - Number(buy)),
     buy_per_share: env_data.buy_price,
     target: env_data.target,
-    company: env_data.name
+    company: env_data.name,
   };
-  
+
   return {
     props: { data },
   };
 };
 
-export function Home ({ data }: Props) {
+export function Home({ data }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -54,15 +57,20 @@ export function Home ({ data }: Props) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          শেয়ার বাজার
-        </h1>
+        <h1 className={styles.title}>শেয়ার বাজার</h1>
 
         <p className={styles.description}>
-          বর্তমানে আমাদের শেয়ার <b> {data.profit_money} টাকা {data.profit} </b>  এ আছে
+          বর্তমানে আমাদের শেয়ার{" "}
+          <b>
+            {" "}
+            {data.profit_money} টাকা {data.profit}{" "}
+          </b>{" "}
+          এ আছে
         </p>
         <h1 className={styles.card2}>
-          <a href="/">আবার চেক করো</a>
+          <Link href="/about">
+            <a>About Us</a>
+          </Link>
         </h1>
         <div className={styles.grid}>
           <div className={styles.card}>
@@ -73,24 +81,32 @@ export function Home ({ data }: Props) {
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2> এখন বিক্রি করলে পাওয়া যাবেঃ</h2>
-            <p>{data.sell} টাকা
-            </p>
+            <p>{data.sell} টাকা</p>
           </div>
         </div>
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2> এখন বিক্রি করলে {data.profit} হবেঃ</h2>
-            <p>{data.profit_money} টাকা
-            </p>
+            <p>{data.profit_money} টাকা</p>
           </div>
         </div>
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2> আরো কিছু তথ্যঃ</h2>
-            <p> ১। স্টক কেনা হয়েছেঃ <b>{data.stock}</b>  টি <br></br>
-                ২। কেনার সময় প্রতি শেয়ারের মুল্য ছিলঃ <b> {data.buy_per_share}</b> টাকা <br></br>
-                ৩। বিক্রি করা টার্গেট যখন প্রতি শেয়ারের মুল্য হবেঃ <b>{data.target}</b> টাকা <br></br>
-                ৪। <b>{data.target}</b>  টাকায় প্রতি শেয়ার বিক্রি করলে পাওয়া যাবে ({data.stock} * {data.target} - ভ্যাট {data.vat} ) = <b>{((data.stock*data.target)-data.vat).toLocaleString()}</b>  টাকা <br></br>
+            <p>
+              {" "}
+              ১। স্টক কেনা হয়েছেঃ <b>{data.stock}</b> টি <br></br>
+              ২। কেনার সময় প্রতি শেয়ারের মুল্য ছিলঃ <b>
+                {" "}
+                {data.buy_per_share}
+              </b>{" "}
+              টাকা <br></br>
+              ৩। বিক্রি করা টার্গেট যখন প্রতি শেয়ারের মুল্য হবেঃ{" "}
+              <b>{data.target}</b> টাকা <br></br>
+              ৪। <b>{data.target}</b> টাকায় প্রতি শেয়ার বিক্রি করলে পাওয়া যাবে (
+              {data.stock} * {data.target} - ভ্যাট {data.vat} ) ={" "}
+              <b>{(data.stock * data.target - data.vat).toLocaleString()}</b>{" "}
+              টাকা <br></br>
             </p>
           </div>
         </div>
@@ -103,12 +119,11 @@ export function Home ({ data }: Props) {
           rel="noopener noreferrer"
         >
           Created By @Sanjib Kumar Sen
-          <span className={styles.logo}>
-          </span>
+          <span className={styles.logo}></span>
         </a>
       </footer>
     </div>
   );
-};
+}
 
 export default Home;
--- scripts/get-env-data.ts
+++ scripts/get-env-data.ts
@@ -3,11 +3,11 @@ import 'dotenv/config'
 export default function get_env_data() {
     const data = {
         name: process.env.TRADING_CODE,
-        stock : process.env.STOCKS,
-        buy_price : process.env.BUY_PRICE_PER_STOCK,
+        stock: process.env.STOCKS,
+        buy_price: process.env.BUY_PRICE_PER_STOCK,
         buy: process.env.TOTAL_BUY,
         vat: process.env.VAT,
         target: process.env.TARGET
-    }    
+    }
     return data
 }
